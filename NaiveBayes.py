ea49c3fb3f968012826bd48c0b656113e78a8875
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-01-18 10:47:29 +0200

    added naivebayes file, started

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -3,8 +3,13 @@ import pandas as pd
 import math
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-url='https://sharon.srworkspace.com/ml/datasets/hw1/cyber_train.csv'
-df=pd.read_csv(url)
-tempTxt=df.drop('sentiment',axis=1).to_numpy()
-texAll=[row[0].split() for index,row in enumerate(tempTxt)]
-
+def readTrainData(file_name):
+    url='https://sharon.srworkspace.com/ml/datasets/hw1/cyber_train.csv'
+    df=pd.read_csv(url)
+    tempTxt=df.drop('sentiment',axis=1).to_numpy()
+    texAll=[row[0].split() for index,row in enumerate(tempTxt)]
+    voc=[word for line in texAll for word in line]
+    lbAll=[lbl for lbl in df['sentiment']]
+    voc=set(voc)
+    cat=set(lbAll)
+    return texAll, lbAll, voc, cat
