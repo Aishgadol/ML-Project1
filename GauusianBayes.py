68f0d698b7c15b5546acbeac3798c45b7758c605
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-01-23 15:53:55 +0200

    working on gaussian bayes file

--- GauusianBayes.py
+++ GauusianBayes.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 df=pd.read_csv('https://sharon.srworkspace.com/ml/datasets/hw1/wine.data.csv')
 #print(df.shape)
 #df.head(5)
+
+def plotDensities():
+    df.plot(kind='density', subplots=True, layout=(4, 4), figsize=(18, 15), sharex=False)
+    plt.show()
+
