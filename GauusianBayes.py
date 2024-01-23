4009b0511d548ebf48496c5b66d305b4586f7a38
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-01-23 15:52:55 +0200

    added files for gaussian bayes

---
+++ GauusianBayes.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df=pd.read_csv('https://sharon.srworkspace.com/ml/datasets/hw1/wine.data.csv')
+#print(df.shape)
+#df.head(5)
