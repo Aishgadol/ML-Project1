ac72a1ac65c424cac066ec988ae5d1a98772d93a
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-01-25 12:48:18 +0200

    more work, progress

--- GaussianBayes.py
+++ GaussianBayes.py
@@ -7,10 +7,28 @@ df=pd.read_csv('https://sharon.srworkspace.com/ml/datasets/hw1/wine.data.csv')
 #print(df.shape)
 #df.head(5)
 
+
 def plotDensities():
     df.plot(kind='density', subplots=True, layout=(4, 4), figsize=(18, 15), sharex=False)
     plt.show()
-
 data=df.drop('Class',axis=1).to_numpy()
 labels=df['Class'].to_numpy()
+cat=set(labels)
+#print(df.drop('Class',axis=1))
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=25)
+priors=np.bincount(y_train)[1:]/len(y_train)
+samples_in_class={label : X_train[y_train==label] for label in cat}
+means={label :np.zeros((len(data[0]))) for label in cat}
+for label in cat:
+    means[label]=np.mean(samples_in_class[label],axis=0)
+
+test_means={label :np.mean(samples_in_class[label],axis=0)  for label in cat}
+for i in cat:
+    print(means[i]-test_means[i])
+#normal covariance, not naive
+class_covariance={label:np.cov(samples_in_class[label]) for label in cat}
+
+#naive gaus 'covariance' (diagonal of variances)
+naive_cov={label: np.diag(np.var(samples_in_class[label], axis=1))for label in cat}
+
+
