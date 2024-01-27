c8e4db57403789c1e356ce4a58df1cceeb583828
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-01-27 19:57:41 +0200

    more work, progress

--- GaussianBayes.py
+++ GaussianBayes.py
@@ -14,7 +14,6 @@ def plotDensities():
 data=df.drop('Class',axis=1).to_numpy()
 labels=df['Class'].to_numpy()
 cat=set(labels)
-#print(df.drop('Class',axis=1))
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=25)
 priors=np.bincount(y_train)[1:]/len(y_train)
 samples_in_class={label : X_train[y_train==label] for label in cat}
@@ -29,16 +28,18 @@ class_cov={label:np.cov(samples_in_class[label],rowvar=False) for label in cat}
 #naive gaus 'covariance' (diagonal of variances)
 naive_cov={label: np.diag(np.var(samples_in_class[label], axis=0))for label in cat}
 
-
+x=X_test[0]
+print(f' shapes: ((x - means[label]).T).shape={((x - means[1]).T).shape}\nnp.linalg.inv(class_cov[label])={np.linalg.inv(class_cov[1]).shape}'
+      f'\n(x-means[label]).shape={(x-means[1]).shape} ')
 def classify_point_gaussian_bayes(x):
-    #prob_per_class={label: np.log((1.0 / np.sqrt(((2 * np.pi) ** len(x)) * np.linalg.det(class_cov[label])))*np.exp(-0.5 * ((x-means[label]).T) @ np.linalg.inv(class_cov[label]) @ (x-means[label]))) for label in cat}
-    prob_per_class = {label: -0.5 * ((x - means[label]).T @ np.linalg.inv(class_cov[label]) @ (x-means[label])) - 0.5 * np.log(
+    prob_per_class = {label: -0.5 * ((x - means[label]).T @ np.linalg.inv(class_cov[label]) @ (x-means[label]))
+                             - 0.5 * np.log(
       np.linalg.det(class_cov[label])) + np.log(priors[label-1]) for label in cat}
     return max(prob_per_class,key=prob_per_class.get)
 
 def classify_point_gaussian_naive_bayes(x):
-    prob_per_class = {label: -0.5 * ((x - means[label]).T @ np.linalg.inv(class_cov[label]) @ (x - means[label])) - 0.5 * np.log(
-        np.linalg.det(naive_cov[label])) + np.log(priors[label - 1]) for label in cat}
+    prob_per_class = {label: -0.5 * ((x - means[label]).T @ np.linalg.inv(naive_cov[label]) @ (x - means[label]))
+                             - 0.5 * np.log(np.linalg.det(naive_cov[label])) + np.log(priors[label - 1]) for label in cat}
     return max(prob_per_class, key=prob_per_class.get)
 
 res = []
